e24a4cd3b169a1f969b99b3fe2d61767d87709d7
AlexeyProjects <62065851+AlexeyProjects@users.noreply.github.com>
2020-09-01 03:32:05 +0400

    releas
    1

---
+++ StorePad/js/script.js
@@ -0,0 +1,50 @@
+var rotate = 0;
+
+$(".t1").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt1").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t2").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt2").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t3").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt3").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t4").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt4").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t5").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt5").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t6").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt6").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t7").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt7").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
+$(".t8").click(function(){
+  rotate = rotate + 180;
+  $(".iconDownt8").css({
+  "transform": "rotate("+rotate+"deg)"
+  });
+});
